301105c9d12d59a2016c6ff38d94a2dacfe31489
Irina Sadovskaya <oderzhanie@gmail.com>
2021-01-18 16:17:48 +0300

    Добавляет планшетную и дескотпную верстку для главной страницы

--- source/js/index.js
+++ source/js/index.js
@@ -2,17 +2,23 @@ const menuButton = document.querySelector(`.main-nav__toggle`);
 const menuButtonText = menuButton.querySelector(`span`);
 const mainNavWrapper = document.querySelector(`.main-nav__wrapper`);
 
+// mainNavWrapper.classList.add(`visually-hidden`);
+menuButton.classList.remove(`visually-hidden`);
+
 buttonClickHandler = () => {
   mainNavWrapper.classList.toggle(`visually-hidden`);
+  // mainNavWrapper.removeProperty('.visually-hidden');
 
   if (menuButton.classList.contains(`main-nav__toggle--open`)) {
     menuButton.classList.remove(`main-nav__toggle--open`);
     menuButton.classList.add(`main-nav__toggle--close`);
     menuButtonText.textContent = `Закрыть меню`;
+    // mainNavWrapper.removeProperty('.visually-hidden');
   } else {
     menuButton.classList.remove(`main-nav__toggle--close`);
     menuButton.classList.add(`main-nav__toggle--open`);
     menuButtonText.textContent = `Открыть меню`;
+    // mainNavWrapper.addProperty('.visually-hidden');
   }
 }
 
